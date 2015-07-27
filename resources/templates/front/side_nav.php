821653585be65dde76853397b517b9b8acb71892
Robin <robin@Robinss-Mac.local>
2015-07-26 22:39:02 -0700

    initialize project

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -8,6 +8,10 @@
     		$query = "SELECT * FROM categories";
     		$send_query = mysqli_query($connection, $query);
 
+            if(!$send_query){
+                die("QUERY FAILED" . mysqli_error($connection));
+            }
+
     		while($row = mysqli_fetch_array($send_query)){
     			echo "<a href='' class='list-group-item'>{$row['cat_title']}</a>";
     		}
